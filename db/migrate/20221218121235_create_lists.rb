0186170cc443e76d8f979a270834e5858303874b
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-18 12:22:17 +0000

    [Finish] 1章 アプリケーションを完成させよう [モデルの作成]

--- db/migrate/20221218121235_create_lists.rb
+++ db/migrate/20221218121235_create_lists.rb
@@ -1,7 +1,9 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-
+      # 下2行を追加
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
