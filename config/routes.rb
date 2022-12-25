ff17a02338beae9bf5a688a69a7f560a76fbe098
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-25 04:10:43 +0000

    [Finish] 4章 詳細画面を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'   #ここを追記します
-  get 'lists' => 'lists#index'
-  # .../list/1 や .../list/3 に該当する
-  get 'lists/:id' => 'lists#show' # 詳細画面を表示
   get 'lists/edit'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'   #ここを追記します
+  get 'lists' => 'lists#index' # 一覧画面
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show', as: 'list' # 詳細画面
+
 
 
 end
