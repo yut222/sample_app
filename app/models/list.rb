b9172fa00ee71519131c453a8b1ea8f630ecc42a
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-25 08:03:48 +0000

    [Start] 11章 レイアウトを整えよう

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,7 @@ class List < ApplicationRecord
   
   validates :title, presence: true #対象項目：title,存在の有無：true(存在しなければならない)
   validates :body, presence: true
-  validates :image, presence: true
+
+  #rails cで画像を扱う場合は複雑になるためコメントアウト
+  # validates :image, presence: true
 end
