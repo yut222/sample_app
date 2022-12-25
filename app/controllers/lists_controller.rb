5c87c1cad5222ad3d470c70de6fdc52474737504
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-25 07:19:18 +0000

    [Finish] 8章 バリデーションの設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,13 +7,16 @@ class ListsController < ApplicationController
     # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
     # 4. トップ画面へリダイレクト×
     # redirect_to '/top' を削除して、以下コードに変更
     # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
   has_one_attached :image
+  
+  validates :title, presence: true #対象項目：title,存在の有無：true(存在しなければならない)
+  validates :body, presence: true
+  validates :image, presence: true
 end
