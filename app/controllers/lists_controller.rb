e0ead20b89a33c4d9f98d59fa67f65ad84f44770
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-25 04:57:40 +0000

    [Start] 6章 画像投稿機能を作ろう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,17 +10,28 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 4. トップ画面へリダイレクト×
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  get 'lists/edit'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'   #ここを追記します
   get 'lists' => 'lists#index' # 一覧画面
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list' # 詳細画面
-
-
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 
 end
